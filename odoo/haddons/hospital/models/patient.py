3a28d6f262539c623ca57cd7cea9fbf215e1d5e5
Felicitydev <feliciteteghomo@gmail.com>
2024-05-25 10:53:31 +0100

    [IMP] Create computed fields

--- odoo/haddons/hospital/models/patient.py
+++ odoo/haddons/hospital/models/patient.py
@@ -1,5 +1,6 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
+from datetime import date
 
 
 class HospitalPatient(models.Model):
@@ -8,7 +9,17 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string="Nom", tracking=True)
+    date_of_birth = fields.Date(string="Date de naissance")
     ref = fields.Char(string="Code", tracking=True)
-    age = fields.Integer(string="Age", tracking=True)
+    age = fields.Integer(string="Age", compute = '_compute_age', tracking=True, store = True)
     gender = fields.Selection([('mal', 'Masculin'), ('female', 'Féminin')], string="Genre", tracking=True)
     active=fields.Boolean(string="Atif", default=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year  
+            else:
+                rec.age = 0   
