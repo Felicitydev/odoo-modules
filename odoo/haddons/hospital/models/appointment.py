91c8f0f1677061fccf8b638d161af1e97c7a0b45
Felicitydev <feliciteteghomo@gmail.com>
2024-07-09 03:06:01 +0100

    [IMP] Hide One2many column based on parent record

--- odoo/haddons/hospital/models/appointment.py
+++ odoo/haddons/hospital/models/appointment.py
@@ -27,6 +27,7 @@ class HospitalAppointment(models.Model):
         ('cancel', 'Annulé')], default='draft', string="Statut", required=True)
     doctor_id = fields.Many2one('res.users', string = "Docteur", tracking=True)
     pharmacy_line_ids = fields.One2many('appointment.pharmacy.lines', 'appointment_id', string="Lignes de pharmacy")
+    hide_sales_price = fields.Boolean(string="Masquer le pdv")
 
 
     @api.onchange('patient_id')
