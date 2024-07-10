4b9c012875bc0e717d7635a618c68ef90a28a2d4
Felicitydev <feliciteteghomo@gmail.com>
2024-07-10 03:07:07 +0100

    [IMP] Create transient model

---
+++ odoo/haddons/hospital/wizard/cancel_appointment.py
@@ -0,0 +1,8 @@
+from odoo import api, fields, models
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = 'cancel.appointment.wizard'
+    _description = 'Cancel Appointment Wizard'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    
+    appointment_id = fields.Many2one('hospital.appointment', string="Rendez-vous")
