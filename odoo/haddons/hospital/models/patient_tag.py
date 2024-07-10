06ed1a64f6d76eea7accb69eea63121204d66e50
Felicitydev <feliciteteghomo@gmail.com>
2024-07-10 01:58:22 +0100

    [IMP] Add color picker widget to fields

--- odoo/haddons/hospital/models/patient_tag.py
+++ odoo/haddons/hospital/models/patient_tag.py
@@ -7,4 +7,4 @@ class HospitalPatient(models.Model):
     
     name = fields.Char(string="Name", required=True)
     active = fields.Boolean(string="Actif", default=True)
-    
+    color = fields.Integer(string="Couleur")
