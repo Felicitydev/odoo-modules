59f5cf413ffd8865e34ce791a54cb28eed7f5986
Felicitydev <feliciteteghomo@gmail.com>
2024-07-12 00:57:29 +0100

    Save files manually

--- odoo/haddons/odoo_inheritance/models/sale_order.py
+++ odoo/haddons/odoo_inheritance/models/sale_order.py
@@ -1,18 +1,9 @@
-# -*- coding: utf-8 -*-
+-*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class odoo_inheritance(models.Model):
-#     _name = 'odoo_inheritance.odoo_inheritance'
-#     _description = 'odoo_inheritance.odoo_inheritance'
+class SaleOrder(models.Model):
+    _inherit= 'sale.order'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
