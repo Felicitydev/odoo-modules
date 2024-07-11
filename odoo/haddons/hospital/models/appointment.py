924ef0094281dada1a753568780bc0ea738df3ea
Felicitydev <feliciteteghomo@gmail.com>
2024-07-12 00:09:49 +0100

    [IMP] Launch wizard from button

--- odoo/haddons/hospital/models/appointment.py
+++ odoo/haddons/hospital/models/appointment.py
@@ -52,8 +52,10 @@ class HospitalAppointment(models.Model):
             rec.state ='done'
             
     def action_cancel(self):
-        for rec in self:
-            rec.state ='cancel'
+        action = self.env.ref('hospital.action_cancel_appointment').read()[0]
+        return action
+        # for rec in self:
+        #     rec.state ='cancel'
             
     def action_draft(self):
         for rec in self:
