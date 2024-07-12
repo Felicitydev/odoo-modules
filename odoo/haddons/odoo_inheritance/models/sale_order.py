a5b46f0b13e9e54905d1973d7a5b7266b86fdae0
Felicitydev <feliciteteghomo@gmail.com>
2024-07-13 00:54:33 +0100

    [IMP] Inherit a function

--- odoo/haddons/odoo_inheritance/models/sale_order.py
+++ odoo/haddons/odoo_inheritance/models/sale_order.py
@@ -7,3 +7,7 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     confirm_user_id = fields.Many2one('res.users', string="Confirmer l'utilisateur")
+
+    def action_confirm(self):
+        super(SaleOrder,self).action_confirm() #super(classname,args).function_name()
+        self.confirm_user_id = self.env.user.id # Retrieve the current user
