0ccffdf1138f7bf213630573af2ff251b14ee295
Felicitydev <feliciteteghomo@gmail.com>
2024-07-13 01:45:18 +0100

    [IMP] Default get function

--- odoo/haddons/hospital/wizard/cancel_appointment.py
+++ odoo/haddons/hospital/wizard/cancel_appointment.py
@@ -1,12 +1,21 @@
 from odoo import api, fields, models
+import datetime
 
 class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
     _description = 'Cancel Appointment Wizard'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     
+    @api.model 
+    def default_get(self,fields):
+        res =super(CancelAppointmentWizard,self).default_get(fields)
+        res['date_cancel'] = datetime.date.today()
+        return res
+    
     appointment_id = fields.Many2one('hospital.appointment', string="Rendez-vous")
     reason = fields.Text(string="Raison de l'annulation")
+    date_cancel = fields.Date(string="Date de l'annulation")
     
     def action_cancel(self):
         return 
+    
