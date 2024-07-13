dcc663179418209c1b444d50e43b7e4fc09fbfb0
Felicitydev <feliciteteghomo@gmail.com>
2024-07-13 01:04:06 +0100

    [IMP] Override Create method

--- odoo/haddons/hospital/models/patient.py
+++ odoo/haddons/hospital/models/patient.py
@@ -17,6 +17,10 @@ class HospitalPatient(models.Model):
     appointment_id = fields.Many2one('hospital.appointment', string="Rdv")
     image = fields.Image(string="Image")
     tag_ids = fields.Many2many('patient.tag',string="Tags")
+    
+    @api.model
+    def create(self,vals):
+        super(HospitalPatient,self).create(vals)
 
     @api.depends('date_of_birth')
     def _compute_age(self):
