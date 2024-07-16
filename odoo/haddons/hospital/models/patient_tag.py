277a233ff099020293337da04e08760c3204271c
Felicitydev <feliciteteghomo@gmail.com>
2024-07-16 01:36:48 +0100

    [IMP] Sql constraints - fields validations

--- odoo/haddons/hospital/models/patient_tag.py
+++ odoo/haddons/hospital/models/patient_tag.py
@@ -9,3 +9,9 @@ class HospitalPatient(models.Model):
     active = fields.Boolean(string="Actif", default=True)
     color = fields.Integer(string="Couleur")
     color2 = fields.Char(string="Couleur 2")
+    sequence = fields.Integer(string="Sequence")
+    
+    _sql_constraints = [
+        ('name_uniq', 'unique (name,active)', 'Le nom doit etre unique!'),
+        ('check_sequence', 'check (sequence > 0)', 'La sequence doit etre un nombre positif!')
+    ]
