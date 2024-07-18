a3f024443483226e39940c4e8b0590a5609b6221
Felicitydev <feliciteteghomo@gmail.com>
2024-07-18 02:54:34 +0100

    [ADD] Add rec name for operation model

--- odoo/haddons/hospital/models/operation.py
+++ odoo/haddons/hospital/models/operation.py
@@ -3,6 +3,7 @@ from odoo import fields, models, api, _
 class HospitalOperation(models.Model):
     _name = 'hospital.operation'
     _description = 'Opéraions Hopital'
+    _rec_name = 'doctor_id'
     _log_access = False # Remove fields create_date, create_uid, write_date write_uid
     
     doctor_id = fields.Many2one('res.users', string = "Docteur")
