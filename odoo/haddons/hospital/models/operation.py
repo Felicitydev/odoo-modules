a222a4fe772e0ffffa1e90007a742fd2ba2e6e3e
Felicitydev <feliciteteghomo@gmail.com>
2024-07-19 02:23:18 +0100

    [IMP] Define refernce field

--- odoo/haddons/hospital/models/operation.py
+++ odoo/haddons/hospital/models/operation.py
@@ -3,11 +3,13 @@ from odoo import fields, models, api, _
 class HospitalOperation(models.Model):
     _name = 'hospital.operation'
     _description = 'Opéraions Hopital'
-    _rec_name = 'doctor_id'
+    _rec_name = 'operation_name'
     _log_access = False # Remove fields create_date, create_uid, write_date write_uid
     
     doctor_id = fields.Many2one('res.users', string = "Docteur")
     operation_name = fields.Char(string="Nom")
+    refernce_record = fields.Reference(selection=[('hospital.patient', 'Patients'),
+                                                  ('hospital.appointment', 'Rendez-vous')] ,string="Record")
     
     @api.model 
     def name_create(self,name):
