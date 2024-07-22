f289636a27bae8115179dd691b07d28bc6ea5e7e
Felicitydev <feliciteteghomo@gmail.com>
2024-07-22 18:36:06 +0100

    [IMP] Add widet handle | drag and change records in tree view

--- odoo/haddons/hospital/models/operation.py
+++ odoo/haddons/hospital/models/operation.py
@@ -5,11 +5,13 @@ class HospitalOperation(models.Model):
     _description = 'Opéraions Hopital'
     _rec_name = 'operation_name'
     _log_access = False # Remove fields create_date, create_uid, write_date write_uid
+    _order = 'sequence,id' # save the range when using drag records
     
     doctor_id = fields.Many2one('res.users', string = "Docteur")
     operation_name = fields.Char(string="Nom")
     refernce_record = fields.Reference(selection=[('hospital.patient', 'Patients'),
                                                   ('hospital.appointment', 'Rendez-vous')] ,string="Record")
+    sequence = fields.Integer(string="Séquence",default=10)
     
     @api.model 
     def name_create(self,name):
